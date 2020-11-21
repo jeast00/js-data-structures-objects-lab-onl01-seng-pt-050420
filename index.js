9cccd9bdeadff3f1ca808a149dff65eab270da07
Jonathan East <jonathan_east@ymail.com>
2020-11-21 18:54:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   return driver;
 }
 
-function deleteFromDriverByKey(driver, key, value) {
+function deleteFromDriverByKey(driver, key) {
   const newObj = Object.assign({}, driver);
   
   delete newObj[key];
